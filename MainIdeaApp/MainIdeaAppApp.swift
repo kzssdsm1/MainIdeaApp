6184cd23c95e109876afa10b86325049c0fa79e7
kzssdsm1 <obaddonsoul@gmail.com>
2022-11-10 15:26:15 +0900

    first commit

---
+++ MainIdeaApp/MainIdeaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainIdeaAppApp.swift
+//  MainIdeaApp
+//
+//  Created by 佐渡島和志 on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct MainIdeaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
