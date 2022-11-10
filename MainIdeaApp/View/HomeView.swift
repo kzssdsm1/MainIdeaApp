481d6900e05308150f42a4acc4d2ae684a8e0d84
kzssdsm1 <obaddonsoul@gmail.com>
2022-11-10 15:34:44 +0900

    ファイルの作成

---
+++ MainIdeaApp/View/HomeView.swift
@@ -0,0 +1,14 @@
+//
+//  HomeView.swift
+//  MainIdeaApp
+//
+//  Created by 佐渡島和志 on 2022/11/10.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
