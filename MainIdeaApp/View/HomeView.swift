884034da56f23f2edd8884aed7050abd86bb8b6f
kzssdsm1 <obaddonsoul@gmail.com>
2022-11-18 14:59:46 +0900

    コードを追加

--- MainIdeaApp/View/HomeView.swift
+++ MainIdeaApp/View/HomeView.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var isActiveDescriptionView = false
+    @State private var path: [NavigationDestination] = []
     
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $path) {
             VStack(spacing: 0) {
                 Image(systemName: "questionmark.circle")
                     .resizable()
