ffd432d2e1ee105a69dc2a0e11fdeffce6225c9b
kzssdsm1 <obaddonsoul@gmail.com>
2023-01-16 16:30:17 +0900

    コードを若干修正

--- MainIdeaApp/Extension/String+Extension.swift
+++ MainIdeaApp/Extension/String+Extension.swift
@@ -9,37 +9,34 @@ import UIKit
 
 extension String {
     func createRuby(color: UIColor = .black) -> NSAttributedString {
-        let paragraphStyle = NSMutableParagraphStyle()
-        
-        paragraphStyle.lineBreakMode = .byCharWrapping
-        
         let textWithRuby = replacingOccurrences(of: "(｜.+?《.+?》)", with: ",$1,", options: .regularExpression)
             .components(separatedBy: ",")
             .map { component -> NSAttributedString in
                 guard component.range(of: "｜(.+?)《(.+?)》", options: .regularExpression, range: nil, locale: nil) != nil else {
                     return NSAttributedString(string: component)
                 }
-
+                
                 let baseText = component.replacingOccurrences(of: "｜(.+?)《.+?》", with: "$1", options: .regularExpression)
                 let rubyText = component.replacingOccurrences(of: "｜.+?《(.+?)》", with: "$1", options: .regularExpression)
-
+                
                 let rubyAttribute: [CFString: Any] = [
                     kCTRubyAnnotationSizeFactorAttributeName: 0.5,
                     kCTForegroundColorAttributeName: color
                 ]
-                // 必要に応じて属性を設定する
+                
                 let rubyAnnotation = CTRubyAnnotationCreateWithAttributes(
                     .auto, .auto, .before, rubyText as CFString, rubyAttribute as CFDictionary
                 )
-
-                return NSAttributedString(string: baseText,
-                                          attributes: [kCTRubyAnnotationAttributeName as NSAttributedString.Key: rubyAnnotation, NSAttributedString.Key.paragraphStyle: paragraphStyle])
+                
+                let attr = [kCTRubyAnnotationAttributeName as NSAttributedString.Key: rubyAnnotation]
+                
+                return NSAttributedString(string: baseText, attributes: attr)
             }
             .reduce(NSMutableAttributedString()) {
                 $0.append($1)
                 return $0
             }
-
+        
         return textWithRuby
     }
 }
--- MainIdeaApp/View/TestView.swift
+++ MainIdeaApp/View/TestView.swift
@@ -8,16 +8,12 @@
 import SwiftUI
 
 struct TestView: View {
-    private let paragraphStyle = NSMutableParagraphStyle()
-    
     var body: some View {
-        VStack {
-            RubyLabelRepresentable(
-                attributedText: "おばあちゃんは白いネコをひざに｜乗《の》せて｜微笑《ほほえ》んでいる".createRuby(),
-                font: .systemFont(ofSize: 24),
-                textColor: .black,
-                textAlignment: .center
-            )
-        }
-    }
+        RubyLabelRepresentable(
+            attributedText: "おばあちゃんは白いネコをひざに｜乗《の》せて｜微笑《ほほえ》んでいる".createRuby(),
+            font: .systemFont(ofSize: 24),
+            textColor: .black,
+            textAlignment: .center
+        )
+    } // body
 }
